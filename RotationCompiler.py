9f5563efa0b27956785181996097a312e753af2b
petros-stougiannidis <petros.stougiannidis@live.de>
2023-03-22 22:12:08 +0100

    refactored and added function for computing the circuit size

--- RotationCompiler.py
+++ RotationCompiler.py
@@ -1,9 +1,6 @@
 import numpy as np
 from itertools import combinations
 
-from collections import OrderedDict
-
-
 class RotationCircuit():
     
     def __init__(self, fractional_values, function):
@@ -33,16 +30,13 @@ class RotationCircuit():
                     for control_qubits_in_higher_layer in circuit[layer_above].keys():
                         if control_qubits_in_lower_layer.issubset(control_qubits_in_higher_layer):
                             circuit[layer_above][control_qubits_in_higher_layer] -= rotation_value
-            
-        self.circuit = circuit
 
-        # update circuit size
-        self.toffoli_count = 0
-        self.ancilla_count = 0
-        for layer, circuit in enumerate(self.circuit[2:]):
-            self.toffoli_count += 2*(layer+1)*len(circuit)
-            if len(circuit) > 0:
-                self.ancilla_count = layer+1
+        # reassemble and store the circuit
+        self.circuit = dict()
+        for layer in circuit:
+            self.circuit.update(layer)
+        
+        self.update_circuit_size()
 
     # generator for iterating over all possible inputs {}, {0}, ..., {0,1,...,n-1} for the quantum algorithm
     def possible_inputs(self,register_size):
@@ -50,16 +44,19 @@ class RotationCircuit():
         for hamming_weight in range(0,register_size+1):
             for input in combinations(input_bits, hamming_weight):
                 yield frozenset(input)
+    
+    def update_circuit_size(self):
+        self.toffoli_count = np.sum([2*(len(control_qubits)-1) for control_qubits in self.circuit.keys()])
+        self.ancilla_count = np.max([len(control_qubits) for control_qubits in self.circuit.keys()])-1
 
     def approximate_up_to_an_error_of(self, error_upper_bound):
         
         # seperate the cheap from the expensive gates and sort the latter according to a contribution-to-cost ratio
-        expensive_gates = dict()
-        for layer in self.circuit[2:]:
-            expensive_gates.update(layer)
+        cheap_gates = dict([gate for gate in self.circuit.items() if len(gate[0]) < 2])
+        expensive_gates = dict([gate for gate in self.circuit.items() if len(gate[0]) >= 2])
         approximate_circuit = sorted(expensive_gates.items(), key = lambda gate: abs(gate[1]/(2*(len(gate[0])-1))))
         
-        # Until the limit on the acceptable error is reached, omit the gate with the least impact
+        # Until the limit on the acceptable error is reached, omit gates with a small contribution-to-cost ratio
         current_error = 0
         while(len(approximate_circuit) > 0):
             error_contribution = abs(approximate_circuit[0][1])
@@ -69,14 +66,11 @@ class RotationCircuit():
             else:
                 break
 
-        # determine the toffoli count of the approximate circuit
-        self.toffoli_count = np.sum([2*(len(control_qubits)-1) for control_qubits,_ in approximate_circuit])
-        self.ancilla_count = np.max([len(control_qubits) for control_qubits,_ in approximate_circuit])-1
-
         # store the entire approximate circuit
-        self.approximate_circuit = {**self.circuit[0],**self.circuit[1],**dict(approximate_circuit)}
+        self.circuit = {**cheap_gates,**dict(approximate_circuit)}
+        self.update_circuit_size()
         
 
 c = RotationCircuit([2**(i) for i in range(3)],lambda x: x*x*x)
 c.approximate_up_to_an_error_of(0)
-print(c.approximate_circuit)
+print(c.circuit)
